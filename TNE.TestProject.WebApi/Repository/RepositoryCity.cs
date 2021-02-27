8b5a7d171eafb7d8f3412c1257c1c5622a792217
Sergey Shishkin <shishkin.s.a@gmail.com>
2021-02-27 09:24:12 +0300

    Init project

---
+++ TNE.TestProject.WebApi/Repository/RepositoryCity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TNE.TestProject.WebApi.Repository
+{   
+    /// <summary>
+    /// Репозитой городов для получения
+    /// по ним температуры
+    /// </summary>
+    public class RepositoryCity
+    {
+        private List<string> _citys = new  List<string>()
+        {
+            "Москва", "Владимир", "Ковров", "Псков", "Минск"
+        };
+
+        public List<string> Citys => _citys;
+
+        public void AddCity(string name) => _citys.Add(name);
+    }
+}
